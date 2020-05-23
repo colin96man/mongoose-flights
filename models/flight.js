574709e19ad705a028376b5fb64a60dfbd52d4f0
Colin Nelson <colinnelson1996@gmail.com>
2020-05-23 10:43:09 -0600

    part 1 complete and embedded schema

---
+++ models/flight.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const destinationSchema = new Schema({
+    airport: String,
+    arrival: Date
+});
+
+const flightSchema = new Schema({
+    airline: String,
+    airport: {
+        type: String,
+        default: function() {
+            return 'DEN'
+        }
+    },
+    flightNo: {type:Number, min: 10, max: 9999},
+    destinations: [destinationSchema],
+    departs: {
+        type: Date,
+        default: function() {
+            const dt = new Date();
+            return dt.setFullYear(dt.getFullYear() + 1);
+        }
+    }
+});
+
+module.exports = mongoose.model('Flight', flightSchema);
